06466039fbc2ad7fe35ffc12fa58e7402cad36fe
Carl Ortutay <carlorty@gmail.com>
2020-08-06 18:52:16 -0400

    some psuedo code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+
+
+  rollTwentySide = (min,max) => {}
+
+  rollTwelveSide = (min,max) => {}
+
+  rollTenSide = (min,max) => {}
+
+  rollNineSide = (min,max) => {}
+
+  rollTwentySide = (min,max) => {}
+
+  rollSixSide = (min,max) => {}
+
+  rollFourSide = (min,max) => {}
+
+  rollPercentage = (min, max) => {}
+
   return (
     <div className="App">
       <h1>Let's Roll!</h1>
