c16c21fc335e3532f6c8a2697b90a281add1f22b
Carl Ortutay <carlorty@gmail.com>
2020-08-06 19:03:16 -0400

    added roll functions for each die

--- src/App.js
+++ src/App.js
@@ -2,29 +2,47 @@ import React from 'react';
 import './App.css';
 
 function App() {
-
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  rollTwentySide = (min,max) => {
+    min = Math.ceil(1);
+    max = Math.floor(20);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  rollTwelveSide = (min,max) => {
+    min = Math.ceil(1);
+    max = Math.floor(12);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  rollTwentySide = (min,max) => {}
+  rollTenSide = (min,max) => {
+    min = Math.ceil(0);
+    max = Math.floor(9);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  rollTwelveSide = (min,max) => {}
+  rollEightSide = (min,max) => {
+    min = Math.ceil(1);
+    max = Math.floor(8);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  rollTenSide = (min,max) => {}
+  rollSixSide = (min,max) => {
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  rollNineSide = (min,max) => {}
+  rollFourSide = (min,max) => {
+    min = Math.ceil(1);
+    max = Math.floor(4);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-  rollTwentySide = (min,max) => {}
-
-  rollSixSide = (min,max) => {}
-
-  rollFourSide = (min,max) => {}
-
-  rollPercentage = (min, max) => {}
+  rollPercentage = (min, max) => {
+    min = Math.ceil(10);
+    max = Math.floor(100);
+    return Math.floor(Math.random()*11)*10
+  }
 
   return (
     <div className="App">
